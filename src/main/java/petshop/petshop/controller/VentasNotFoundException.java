fa7d6180b6ce818ff3d6458fe492616136664989
BJ505 <byronjaramilloquezada@gmail.com>
2024-04-25 01:37:47 -0400

    Create VentasNotFoundException.java
    Se agrega archivo para manejar excepciones

---
+++ src/main/java/petshop/petshop/controller/VentasNotFoundException.java
@@ -0,0 +1,11 @@
+package petshop.petshop.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class VentasNotFoundException extends RuntimeException{
+    public VentasNotFoundException(String message) {
+        super(message);
+    }
+}
